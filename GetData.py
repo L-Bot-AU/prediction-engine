c53c9ce128337bb294e8345da915690933165b88
JamesTreloar <54793877+JamesTreloar@users.noreply.github.com>
2021-01-22 14:37:53 +1100

    Changed format of equations and added documentation

--- GetData.py
+++ GetData.py
@@ -2,7 +2,9 @@ import predictor as p
 import json
 
 def getData(term, week, day):
-    if term > 4:
+    #First trys to get data from file, if not found calls generator function
+    #Makes sure bad data is not given (probably not needed)
+    if term > 4 or day > 5:
         return
     f = open("days.json")
     days = json.load(f)
@@ -10,5 +12,5 @@ def getData(term, week, day):
     for key in days:
         if key == dayID:
             return days[dayID]
-    
+    #This will get the data
     return p.callData(term, week, day)
--- predictor.py
+++ predictor.py
@@ -1,20 +1,35 @@
 import json
+#Notes
+#Data is either and equation or a specified uppper and lower bounds
+#The equations are no more than degree 2. i.e. ax+c or ax^2+bx+c
+#a and b are always written with one decimal place e.g. 9.8 6.0
+#c is always a whole number
+#This is as detailed as each of these numbers need to be for predictions to work
+#days.json stores each days prediction the keys in it are t-w-d. (Term-week-day)
+#Day here is represented by a numerical value 1-5 for Mon-Fri
+#Variables for each function are named:
+#(t|b)<year>(bs|b1|b2)
+#year = 2018|2019
+#t refers to maximum or top value
+#b refers to minimum or bottom value
+#bs, b1, b2 refer to before school, break 1, break 2 respectively 
+#This is based on previous data
 
 def Term1Mon(week):
     #2018
     t2018bs = 85
     b2018bs = 50
-    t2018b1 = round(5.13*week+27.32)+25
-    b2018b1 = round(5.13*week+27.32)-25
-    t2018b2 = round(2.71*week+10.7)+10
-    b2018b2 = round(2.71*week+10.7)-10
+    t2018b1 = round(5.1*week+27)+25
+    b2018b1 = round(5.1*week+27)-25
+    t2018b2 = round(2.7*week+11)+10
+    b2018b2 = round(2.7*week+11)-10
     #2019
-    t2019bs = round(3.2*week+38.82)+20
-    b2019bs = round(3.2*week+38.82)-20
-    t2019b1 = round(8.36*week+25.54)+30
-    b2019b1 = round(8.36*week+25.54)-30
-    t2019b2 = round(1.52*week+26.49)+20
-    b2019b2 = round(1.52*week+26.49)-20
+    t2019bs = round(3.2*week+39)+20
+    b2019bs = round(3.2*week+39)-20
+    t2019b1 = round(8.4*week+26)+30
+    b2019b1 = round(8.4*week+26)-30
+    t2019b2 = round(1.5*week+26)+20
+    b2019b2 = round(1.5*week+26)-20
 
     maxbs = max(t2018bs,t2019bs)
     minbs = min(b2018bs,b2019bs)
@@ -38,12 +53,12 @@ def Term1Tue(week):
     t2018b2 = 40
     b2018b2 = 20
     #2019
-    t2019bs = round(1.26*week+44.66)+10
-    b2019bs = round(1.26*week+44.66)-10
-    t2019b1 = round(2.26*week+45.87)+20
-    b2019b1 = round(2.26*week+45.87)-20
-    t2019b2 = round(1.77*week+13.69)+15
-    b2019b2 = round(1.77*week+13.69)-15
+    t2019bs = round(1.3*week+45)+10
+    b2019bs = round(1.3*week+45)-10
+    t2019b1 = round(2.3*week+46)+20
+    b2019b1 = round(2.3*week+46)-20
+    t2019b2 = round(1.8*week+14)+15
+    b2019b2 = round(1.8*week+14)-15
 
     maxbs = max(t2018bs,t2019bs)
     minbs = min(b2018bs,b2019bs)
@@ -60,19 +75,19 @@ def Term1Tue(week):
 
 def Term1Wed(week):
     #2018
-    t2018bs = round(1.73*week+44)+30
-    b2018bs = round(1.73*week+44)-30
-    t2018b1 = round(1.98*week+12.3)+15
-    b2018b1 = round(1.98*week+12.3)-15
-    t2018b2 = round(3.43*week+23.5)+20
-    b2018b2 = round(3.43*week+23.5)-20
+    t2018bs = round(1.7*week+44)+30
+    b2018bs = round(1.7*week+44)-30
+    t2018b1 = round(2.0*week+12)+15
+    b2018b1 = round(2.0*week+12)-15
+    t2018b2 = round(3.4*week+24)+20
+    b2018b2 = round(3.4*week+24)-20
     #2019
-    t2019bs = round(5.44*week+26.69)+20
-    b2019bs = round(5.44*week+26.69)-20
-    t2019b1 = round(2.7*week+9.8)+10
-    b2019b1 = round(2.7*week+9.8)-10
-    t2019b2 = round(6.07*week+26.69)+20
-    b2019b2 = round(6.07*week+26.69)-20
+    t2019bs = round(5.4*week+27)+20
+    b2019bs = round(5.4*week+27)-20
+    t2019b1 = round(2.7*week+10)+10
+    b2019b1 = round(2.7*week+10)-10
+    t2019b2 = round(6.0*week+27)+20
+    b2019b2 = round(6.0*week+27)-20
 
     maxbs = max(t2018bs,t2019bs)
     minbs = min(b2018bs,b2019bs)
@@ -89,17 +104,17 @@ def Term1Wed(week):
 
 def Term1Thu(week):
     #2018
-    t2018bs = round(3.34*week+12.3)+15
-    b2018bs = round(3.34*week+12.3)-15
-    t2018b1 = round(1.06*week+11.44)+15
-    b2018b1 = round(1.06*week+11.44)-15
+    t2018bs = round(3.3*week+12)+15
+    b2018bs = round(3.3*week+12)-15
+    t2018b1 = round(1.0*week+11)+15
+    b2018b1 = round(1.0*week+11)-15
     t2018b2 = 25
     b2018b2 = 0
     #2019
     t2019bs = 80
     b2019bs = 40
-    t2019b1 = round(1.68*week+6.45)+10
-    b2019b1 = round(1.68*week+6.45)-10
+    t2019b1 = round(1.7*week+6)+10
+    b2019b1 = round(1.7*week+6)-10
     t2019b2 = 20
     b2019b2 = 0
 
@@ -118,19 +133,19 @@ def Term1Thu(week):
 
 def Term1Fri(week):
     #2018
-    t2018bs = round(3.1*week+82.5)+20
-    b2018bs = round(3.1*week+82.5)-20
-    t2018b1 = round(2.44*week+11.72)+10
-    b2018b1 = round(2.44*week+11.72)-10
-    t2018b2 = round(5.41*week+21.89)+20
-    b2018b2 = round(5.41*week+21.89)-20
+    t2018bs = round(3.1*week+83)+20
+    b2018bs = round(3.1*week+83)-20
+    t2018b1 = round(2.4*week+12)+10
+    b2018b1 = round(2.4*week+12)-10
+    t2018b2 = round(5.4*week+22)+20
+    b2018b2 = round(5.4*week+22)-20
     #2019
-    t2019bs = round(5.47*week+69.4)+20
-    b2019bs = round(5.47*week+69.4)-20
-    t2019b1 = round(3.59*week+10.73)+10
-    b2019b1 = round(3.59*week+10.73)-10
-    t2019b2 = round(3.35*week+37.27)+20
-    b2019b2 = round(3.35*week+37.27)-20
+    t2019bs = round(5.5*week+69)+20
+    b2019bs = round(5.5*week+69)-20
+    t2019b1 = round(3.6*week+11)+10
+    b2019b1 = round(3.6*week+11)-10
+    t2019b2 = round(3.4*week+37)+20
+    b2019b2 = round(3.4*week+37)-20
 
     maxbs = max(t2018bs,t2019bs)
     minbs = min(b2018bs,b2019bs)
@@ -147,10 +162,10 @@ def Term1Fri(week):
 
 def Term2Mon(week):
     #2018
-    t2018bs = round(3.17*week+50.39)+10
-    b2018bs = round(3.17*week+50.39)-10
-    t2018b1 = round(-3.3*week+84.78)+10
-    b2018b1 = round(-3.3*week+84.78)-10
+    t2018bs = round(3.2*week+50)+10
+    b2018bs = round(3.2*week+50)-10
+    t2018b1 = round(-3.3*week+84)+10
+    b2018b1 = round(-3.3*week+84)-10
     t2018b2 = 50
     b2018b2 = 30
     #2019
@@ -180,23 +195,23 @@ def Term2Mon(week):
 
 def Term2Tue(week):
     #2018
-    t2018bs = round(4.82*week+41)+10
-    b2018bs = round(4.82*week+41)-10
-    t2018b1 = round(-5.39*week+84.78)+10
-    b2018b1 = round(-5.39*week+84.78)-10
+    t2018bs = round(4.8*week+41)+10
+    b2018bs = round(4.8*week+41)-10
+    t2018b1 = round(-5.4*week+85)+10
+    b2018b1 = round(-5.4*week+85)-10
     t2018b2 = 50
     b2018b2 = 20
     #2019
     if week <6:
-        t2019bs = round(-14.3*week+112.7)+15
-        b2019bs = round(-14.3*week+112.7)-15
+        t2019bs = round(-13*week+113)+5
+        b2019bs = round(-13*week+113)-5
     else:
-        t2019bs = 100
-        b2019bs = 60
-    t2019b1 = 110
+        t2019bs = 90
+        b2019bs = 80
+    t2019b1 = 80
     b2019b1 = 40
-    t2019b2 = 50
-    b2019b2 = 0
+    t2019b2 = 0
+    b2019b2 = 50
 
     maxbs = max(t2018bs,t2019bs)
     minbs = min(b2018bs,b2019bs)
@@ -220,12 +235,12 @@ def Term2Wed(week):
     t2018b2 = 80
     b2018b2 = 40
     #2019
-    t2019bs = round(6.03*week+24.53)+15
-    b2019bs = round(6.03*week+24.53)-15
-    t2019b1 = round(3.2*week+7.8)+15
-    b2019b1 = round(3.2*week+7.8)-15
-    t2019b2 = round(7.89*week+24.2)+20
-    b2019b2 = round(7.89*week+24.2)-20
+    t2019bs = round(6.0*week+25)+15
+    b2019bs = round(6.0*week+25)-15
+    t2019b1 = round(3.2*week+8)+15
+    b2019b1 = round(3.2*week+8)-15
+    t2019b2 = round(7.9*week+24)+20
+    b2019b2 = round(7.9*week+24)-20
 
     maxbs = max(t2018bs,t2019bs)
     minbs = min(b2018bs,b2019bs)
@@ -242,17 +257,17 @@ def Term2Wed(week):
 
 def Term2Thu(week):
     #2018
-    t2018bs = round(5.16*week+35)+15
-    b2018bs = round(5.16*week+35)-15
+    t2018bs = round(5.2*week+35)+15
+    b2018bs = round(5.2*week+35)-15
     t2018b1 = 20
     b2018b1 = 40
     t2018b2 = 25
     b2018b2 = 0
     #2019
-    t2019bs = round(4.42*week+32.02)+20
-    b2019bs = round(4.42*week+32.02)-20
-    t2019b1 = round(2.35*week+3.72)+10
-    b2019b1 = round(2.35*week+3.72)-10
+    t2019bs = round(4.4*week+32)+20
+    b2019bs = round(4.4*week+32)-20
+    t2019b1 = round(2.4*week+4)+10
+    b2019b1 = round(2.4*week+4)-10
     t2019b2 = 20
     b2019b2 = 0
 
@@ -271,10 +286,10 @@ def Term2Thu(week):
 
 def Term2Fri(week):
     #2018
-    t2018bs = round(8.75*week+51.46)+20
-    b2018bs = round(8.75*week+51.46)-20
-    t2018b1 = round(-1.76*week*week+18.3*week+11.72)+10
-    b2018b1 = round(-1.76*week*week+18.3*week+11.72)-10
+    t2018bs = round(8.8*week+51)+20
+    b2018bs = round(8.8*week+51)-20
+    t2018b1 = round(-1.8*week*week+18*week+12)+10
+    b2018b1 = round(-1.8*week*week+18*week+12)-10
     t2018b2 = 65
     b2018b2 = 40
     #2019
@@ -282,8 +297,8 @@ def Term2Fri(week):
     b2019bs = 60
     t2019b1 = 40
     b2019b1 = 20
-    t2019b2 = round(-5.47*week+66.67)+20
-    b2019b2 = round(-5.47*week+66.67)-20
+    t2019b2 = round(-5.5*week+67)+20
+    b2019b2 = round(-5.5*week+67)-20
     maxbs = max(t2018bs,t2019bs)
     minbs = min(b2018bs,b2019bs)
     maxb1 = max(t2018b1,t2019b1)
@@ -303,15 +318,15 @@ def Term3Mon(week):
     b2018bs = 60
     t2018b1 = 80
     b2018b1 = 70
-    t2018b2 = round(-6.32*week+77.67)+15
-    b2018b2 = round(-6.32*week+77.67)-15
+    t2018b2 = round(-6.3*week+78)+15
+    b2018b2 = round(-6.3*week+78)-15
     #2019
     t2019bs = 90
     b2019bs = 50
     t2019b1 = 90
     b2019b1 = 60
-    t2019b2 = round(4.31*week+23.24)+15
-    b2019b2 = round(4.31*week+23.24)-15
+    t2019b2 = round(4.3*week+23)+15
+    b2019b2 = round(4.3*week+23)-15
 
     maxbs = max(t2018bs,t2019bs)
     minbs = min(b2018bs,b2019bs)
@@ -328,8 +343,8 @@ def Term3Mon(week):
 
 def Term3Tue(week):
     #2018
-    t2018bs = round(3.9*week+54.26)+15
-    b2018bs = round(3.9*week+54.26)-15
+    t2018bs = round(3.9*week+54)+15
+    b2018bs = round(3.9*week+54)-15
     t2018b1 = 90
     b2018b1 = 20
     t2018b2 = round(-3.6*week+56)+15
@@ -338,8 +353,8 @@ def Term3Tue(week):
     #2019
     t2019bs = 100
     b2019bs = 60
-    t2019b1 = round(6.61*week+22.2)+10
-    b2019b1 = round(6.61*week+22.2)-10
+    t2019b1 = round(6.6*week+22)+10
+    b2019b1 = round(6.6*week+22)-10
     t2019b2 = 70
     b2019b2 = 20
 
@@ -362,15 +377,15 @@ def Term3Wed(week):
     b2018bs = 50
     t2018b1 = 50
     b2018b1 = 30
-    t2018b2 = round(1.6*week+46.6)+20
-    b2018b2 = round(1.6*week+46.6)-20
+    t2018b2 = round(1.6*week+47)+20
+    b2018b2 = round(1.6*week+47)-20
     #2019
     t2019bs = 70
     b2019bs = 50
     t2019b1 = 30
     b2019b1 = 0
-    t2019b2 = round(6.71*week+34.4)+20
-    b2019b2 = round(6.71*week+34.4)-20
+    t2019b2 = round(6.7*week+34)+20
+    b2019b2 = round(6.7*week+34)-20
 
     maxbs = max(t2018bs,t2019bs)
     minbs = min(b2018bs,b2019bs)
@@ -387,15 +402,15 @@ def Term3Wed(week):
 
 def Term3Thu(week):
     #2018
-    t2018bs = round(0.93*week+57.2)+10
-    b2018bs = round(0.93*week+57.2)-10
+    t2018bs = round(0.9*week+57)+10
+    b2018bs = round(0.9*week+57)-10
     t2018b1 = 40
     b2018b1 = 20
     t2018b2 = 20
     b2018b2 = 0
     #2019
-    t2019bs = round(3.23*week+34.4)+20
-    b2019bs = round(3.23*week+34.4)-20
+    t2019bs = round(3.2*week+34)+20
+    b2019bs = round(3.2*week+34)-20
     t2019b1 = 50
     b2019b1 = 20
     t2019b2 = 20
@@ -418,17 +433,17 @@ def Term3Fri(week):
     #2018
     t2018bs = 120
     b2018bs = 60
-    t2018b1 = round(4.25*week+50.6)+20
-    b2018b1 = round(4.25*week+50.6)-20
+    t2018b1 = round(4.3*week+51)+20
+    b2018b1 = round(4.3*week+51)-20
     t2018b2 = 80
     b2018b2 = 40
     #2019
-    t2019bs = round(-2.69*week*week+36.6*week+-22)+5
-    b2019bs = round(-2.69*week*week+36.6*week+-22)-5
-    t2019b1 = round(3.59*week+10.73)+10
-    b2019b1 = round(3.59*week+10.73)-10
-    t2019b2 = round(-2.04*week*week+26.6*week+4.4)+20
-    b2019b2 = round(-2.04*week*week+26.6*week+4.4)-20
+    t2019bs = round(-2.7*week*week+36.6*week+-22)+5
+    b2019bs = round(-2.7*week*week+36.6*week+-22)-5
+    t2019b1 = round(3.6*week+10)+10
+    b2019b1 = round(3.6*week+10)-10
+    t2019b2 = round(-2.0*week*week+26.6*week+4)+20
+    b2019b2 = round(-2.0*week*week+26.6*week+4)-20
 
     maxbs = max(t2018bs,t2019bs)
     minbs = min(b2018bs,b2019bs)
@@ -511,8 +526,8 @@ def Term4Wed(week):
     t2018b2 = round(-6.2*week+88)+30
     b2018b2 = round(-6.2*week+88)-30
     #2019
-    t2019bs = round(-4.44*week+76)+20
-    b2019bs = round(-4.44*week+76)+20
+    t2019bs = round(-4.4*week+76)+20
+    b2019bs = round(-4.4*week+76)+20
     t2019b1 = 20
     b2019b1 = 0
     t2019b2 = round(-6.1*week+97)+20
@@ -533,8 +548,8 @@ def Term4Wed(week):
 
 def Term4Thu(week):
     #2018
-    t2018bs = round(-6.6*week+109.4)+20
-    b2018bs = round(-6.6*week+109.4)-20
+    t2018bs = round(-6.6*week+109)+20
+    b2018bs = round(-6.6*week+109)-20
     t2018b1 = 40
     b2018b1 = 20
     t2018b2 = 20
@@ -564,17 +579,17 @@ def Term4Fri(week):
     #2018
     t2018bs = round(-5.2*week+119)+30
     b2018bs = round(-5.2*week+119)-30
-    t2018b1 = round(-3.25*week+47)+10
-    b2018b1 = round(-3.25*week+47)-10
-    t2018b2 = round(-6.61*week+101)+20
-    b2018b2 = round(-6.61*week+101)-20
+    t2018b1 = round(-3.3*week+47)+10
+    b2018b1 = round(-3.3*week+47)-10
+    t2018b2 = round(-6.6*week+101)+20
+    b2018b2 = round(-6.6*week+101)-20
     #2019
-    t2019bs = round(-7.72*week+128.93)+5
-    b2019bs = round(-7.72*week+128.93)-5
-    t2019b1 = round(-4.32*week+60.89)+10
-    b2019b1 = round(-4.32*week+60.89)-10
-    t2019b2 = round(-8.9*week+118.5)+20
-    b2019b2 = round(-8.9*week+118.5)-20
+    t2019bs = round(-7.7*week+129)+5
+    b2019bs = round(-7.7*week+129)-5
+    t2019b1 = round(-4.3*week+61)+10
+    b2019b1 = round(-4.3*week+61)-10
+    t2019b2 = round(-8.9*week+119)+20
+    b2019b2 = round(-8.9*week+119)-20
 
     maxbs = max(t2018bs,t2019bs)
     minbs = min(b2018bs,b2019bs)
@@ -588,55 +603,6 @@ def Term4Fri(week):
             "2019":[t2019bs, b2019bs, t2019b1, b2019b1, t2019b2, b2019b2]}
 
     return return_dict
-
-def generateData(term, week, day):
-    #Get data for a specific day
-    pred = {}
-    if term == 1:
-        if day == 1:
-            pred[str(term)+'-'+str(week)+'-'+str(day)] = Term1Mon(week)
-        elif day == 2:
-            pred[str(term)+'-'+str(week)+'-'+str(day)] = Term1Tue(week)
-        elif day == 3:
-            pred[str(term)+'-'+str(week)+'-'+str(day)] = Term1Wed(week)
-        elif day == 4:
-            pred[str(term)+'-'+str(week)+'-'+str(day)] = Term1Thu(week)
-        elif day == 5:
-            pred[str(term)+'-'+str(week)+'-'+str(day)] = Term1Fri(week)
-    elif term == 2:
-        if day == 1:
-            pred[str(term)+'-'+str(week)+'-'+str(day)] = Term2Mon(week)
-        elif day == 2:
-            pred[str(term)+'-'+str(week)+'-'+str(day)] = Term2Tue(week)
-        elif day == 3:
-            pred[str(term)+'-'+str(week)+'-'+str(day)] = Term2Wed(week)
-        elif day == 4:
-            pred[str(term)+'-'+str(week)+'-'+str(day)] = Term2Thu(week)
-        elif day == 5:
-            pred[str(term)+'-'+str(week)+'-'+str(day)] = Term2Fri(week)
-    elif term == 3:
-        if day == 1:
-            pred[str(term)+'-'+str(week)+'-'+str(day)] = Term3Mon(week)
-        elif day == 2:
-            pred[str(term)+'-'+str(week)+'-'+str(day)] = Term3Tue(week)
-        elif day == 3:
-            pred[str(term)+'-'+str(week)+'-'+str(day)] = Term3Wed(week)
-        elif day == 4:
-            pred[str(term)+'-'+str(week)+'-'+str(day)] = Term3Thu(week)
-        elif day == 5:
-            pred[str(term)+'-'+str(week)+'-'+str(day)] = Term3Fri(week)
-    elif term == 4:
-        if day == 1:
-            pred[str(term)+'-'+str(week)+'-'+str(day)] = Term4Mon(week)
-        elif day == 2:
-            pred[str(term)+'-'+str(week)+'-'+str(day)] = Term4Tue(week)
-        elif day == 3:
-            pred[str(term)+'-'+str(week)+'-'+str(day)] = Term4Wed(week)
-        elif day == 4:
-            pred[str(term)+'-'+str(week)+'-'+str(day)] = Term4Thu(week)
-        elif day == 5:
-            pred[str(term)+'-'+str(week)+'-'+str(day)] = Term4Fri(week)
-    return pred
 
 def callData(term, week, day):
     #Read existing file so do not write over
