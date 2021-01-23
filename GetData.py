c2846ab2c66d1eb58ee7e82c334b9a1621d4d01a
JamesTreloar <54793877+JamesTreloar@users.noreply.github.com>
2021-01-24 10:25:51 +1100

    Modified to return only overall ranges

--- GetData.py
+++ GetData.py
@@ -5,12 +5,12 @@ def getData(term, week, day):
     #First trys to get data from file, if not found calls generator function
     #Makes sure bad data is not given (probably not needed)
     if term > 4 or day > 5:
-        return
+        return "Give me proper data you nong"
     f = open("days.json")
     days = json.load(f)
     dayID = str(term)+'-'+str(week)+'-'+str(day)
     for key in days:
         if key == dayID:
-            return days[dayID]
+            return days[dayID]["Overall"]
     #This will get the data
-    return p.callData(term, week, day)
+    return p.callData(term, week, day)["Overall"]
