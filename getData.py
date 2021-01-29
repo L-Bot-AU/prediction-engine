50b2c23aa1fd869991089a2a81e782ecaf827c7e
JamesTreloar <54793877+JamesTreloar@users.noreply.github.com>
2021-01-29 19:07:34 +1100

    Added docstring for getData function

--- getData.py
+++ getData.py
@@ -1,11 +1,20 @@
 import predictor as p
 import json
 
+
+
 def getData(term, week, day):
+    """
+    :param term: the term (1-4)
+    :param week: the week (of each term)
+    :param day: the weekday represented as an integer( 1 for Monday)
+    :return: an array in the format: `[before school max, before school min, break 1 max, break 1 min, break 2 max, break 2 min]`
+    """
+
     #First trys to get data from file, if not found calls generator function
     #Makes sure bad data is not given (probably not needed)
     if term > 4 or day > 5:
-        return "Give me proper data you nong"
+        return "Give me proper inputs you nong"
     f = open("days.json")
     days = json.load(f)
     dayID = str(term)+'-'+str(week)+'-'+str(day)
@@ -14,3 +23,5 @@ def getData(term, week, day):
             return days[dayID]["Overall"]
     #This will get the data
     return p.callData(term, week, day)[dayID]["Overall"]
+
+print(getData(4,9,3))
