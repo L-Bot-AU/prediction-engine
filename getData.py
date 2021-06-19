5a1fa87a49a2c28a1c85497bdd8b0b7f69ed47b8
JamesTreloar <54793877+JamesTreloar@users.noreply.github.com>
2021-06-19 14:28:37 +1000

    Added functionality for weighting prediction
    Prediction weight functionality has been added. Current weights are a stub.

--- getData.py
+++ getData.py
@@ -1,17 +1,77 @@
 import predictor
 import json
 
+time_interval_count = 3
+
 def getData(term, week, day):
     """
     :param term: the term (1-4)
     :param week: the week (of each term)
     :param day: the weekday represented as an integer( 1 for Monday)
-    :return: an array in the format: `[before school max, before school min, break 1 max, break 1 min, break 2 max, break 2 min]`
+    :return: an array in the format: `[before school , break 1, break 2]`
     """
 
     #Makes sure bad data is not given (probably not needed)
     if term > 4 or day > 5 or week > 13:
          raise Exception("Data provided out of range")
-    dayID = str(term)+'-'+str(week)+'-'+str(day)
-    #This will get the data
-    return predictor.callData(term, week, day)
+    
+    #Obtains prediction
+    prediction = predictor.callData(term, week, day)
+    prediction = weightData(prediction)
+    return prediction
+
+def weightData(initial_prediction):
+     # Weights previous years data based on relevance to current year to improve accuracy of prediction
+     # Example
+     # For T3 W3 Mon Lunch
+     #  ________________________________________
+     # | year       | 2018 | 2019 | 2020 | 2021 |
+     # | prediction |  23  |  18  |  43  |  36  |
+     # | weight     |  3   |  2   |  0   |  4   |
+     # |________________________________________|
+     # From there each prediction will be multiplied by its weight value and summed
+     # 23*4 + 18*2 + 43*0 + 36*4 = 272
+     # This result will be divided by the summ of all the weight values
+     # 3 + 2 + 4 = 9
+     # 272/9 = 30.22 
+     # Therefore predicted value will be 30
+
+     new = {"Jnr":[0,0,0], "Snr":[0,0,0]}
+
+     #Jnr
+     weights = getWeights("Jnr")
+     count = 0
+     #Sums up each (weight*year) for each time period
+     for year in weights:
+          for i in range(0,time_interval_count):
+               new["Jnr"][i] += initial_prediction["Jnr"][year][i]*weights[year]
+          count += weights[year]
+
+     #Divides each summed value for each time period
+     for i in range(0,time_interval_count-1):
+               new["Jnr"][i] = round((new["Jnr"][i]/count))
+
+     #Snr
+     weights = getWeights("Snr")
+     count = 0
+     #Sums up each (weight*year) for each time period
+     for year in weights:
+          for i in range(0,time_interval_count):
+               new["Snr"][i] += initial_prediction["Snr"][year][i]*weights[year]
+          count += weights[year]
+
+     #Divides each summed value for each time period
+     for i in range(0,time_interval_count-1):
+               new["Snr"][i] = round((new["Snr"][i]/count))
+
+     return new
+
+
+#Stub
+def getWeights(library):
+     if library == "Jnr":
+          return {2018:1,2019:2}
+     elif library == "Snr":
+          return {2019:1}
+     else:
+          raise Exception("Incorrect library provided")
--- predictor.py
+++ predictor.py
@@ -12,19 +12,22 @@ def callData(term, week, day):
     calcData = json.load(f)
     f.close()
     
+    return_dict = {"Jnr":{}, "Snr":{}}
+
     jnr_count = 0
     jnr_bs = 0
     jnr_b1 = 0
     jnr_b2 = 0
     for year in range(min_year_jnr,max_year_jnr+1):
         working = calcData["Jnr"][str(year)]["Term"+str(term)][days[day-1]]
-        jnr_bs += working["bs"]["m"]*week + working["bs"]["b"]
-        jnr_b1 += working["b1"]["m"]*week + working["b1"]["b"]
-        jnr_b2 += working["b2"]["m"]*week + working["b2"]["b"]
+        jnr_bs = working["bs"]["m"]*week + working["bs"]["b"]
+        jnr_b1 = working["b1"]["m"]*week + working["b1"]["b"]
+        jnr_b2 = working["b2"]["m"]*week + working["b2"]["b"]
         jnr_count +=1
-    jnr_bs = jnr_bs/jnr_count
-    jnr_b1 = jnr_b1/jnr_count
-    jnr_b2 = jnr_b2/jnr_count
+        return_dict["Jnr"][year] = [jnr_bs,jnr_b1,jnr_b2]
+    # jnr_bs = jnr_bs/jnr_count
+    # jnr_b1 = jnr_b1/jnr_count
+    # jnr_b2 = jnr_b2/jnr_count
 
     snr_count = 0
     snr_bs = 0
@@ -32,14 +35,14 @@ def callData(term, week, day):
     snr_b2 = 0
     for year in range(min_year_snr,max_year_snr+1):
         working = calcData["Snr"][str(year)]["Term"+str(term)][days[day-1]]
-        snr_bs += working["bs"]["m"]*week + working["bs"]["b"]
-        snr_b1 += working["b1"]["m"]*week + working["b1"]["b"]
-        snr_b2 += working["b2"]["m"]*week + working["b2"]["b"]
+        snr_bs = working["bs"]["m"]*week + working["bs"]["b"]
+        snr_b1 = working["b1"]["m"]*week + working["b1"]["b"]
+        snr_b2 = working["b2"]["m"]*week + working["b2"]["b"]
         snr_count +=1
-    snr_bs = snr_bs/snr_count
-    snr_b1 = snr_b1/snr_count
-    snr_b2 = snr_b2/snr_count
+        return_dict["Snr"][year] = [snr_bs,snr_b1,snr_b2]
+    # snr_bs = snr_bs/snr_count
+    # snr_b1 = snr_b1/snr_count
+    # snr_b2 = snr_b2/snr_count
 
-    return_dict = { "Jnr": [jnr_bs, jnr_b1, jnr_b2],
-                    "Snr": [snr_bs, snr_b1, snr_b2]}
+    
     return return_dict
