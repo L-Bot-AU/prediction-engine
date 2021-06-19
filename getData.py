56a8f9c841d1bca575a1cd104e77189852f26f04
JamesTreloar <54793877+JamesTreloar@users.noreply.github.com>
2021-06-19 13:11:01 +1000

    Update getData.py

--- getData.py
+++ getData.py
@@ -6,7 +6,7 @@ def getData(term, week, day):
     :param term: the term (1-4)
     :param week: the week (of each term)
     :param day: the weekday represented as an integer( 1 for Monday)
-    :return: an array in the format: `[before school max, before school min, break 1 max, break 1 min, break 2 max, break 2 min]`
+    :return: an array in the format: `[before school max, break 1, break 2]`
     """
 
     #Makes sure bad data is not given (probably not needed)
